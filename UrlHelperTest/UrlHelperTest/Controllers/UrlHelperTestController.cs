2b9647dad2d9e9d2f7f9fd73d46ca9b201c68d80
Jasmeet Sangari <jasmeet.sangari@gmail.com>
2012-12-12 23:22:46 -0500

    Adding Test Project

---
+++ UrlHelperTest/UrlHelperTest/Controllers/UrlHelperTestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AttributeRouting;
+using UrlHelperTest.Models;
+using AttributeRouting.Web.Http;
+
+namespace UrlHelperTest.Controllers
+{
+    [RoutePrefix("api/v1/users")]
+    public class UrlHelperTestController : ApiController
+    {
+        [GET("?{urlId}", RouteName = "GetByQueryString")]
+        public string GetByQueryString(string urlId)
+        {
+            return "You called GetByQueryString";
+ 
+        }
+         [GET("links")]
+        public List<Link> GetLinks()
+        {
+            var urlHelper = Request.GetUrlHelper();
+            var links = new List<Link> { 
+                                new Link { Name="GetById" , Url = urlHelper.Link("GetById", new { userId = "123" }) }, 
+                                new Link { Name="GetByQueryString" , Url = urlHelper.Link("GetByQueryString", new { urlId = "123" }) }
+         };
+
+            return links;
+        }
+         [GET("{userId}" , RouteName = "GetById")]
+         public string GetById(string userId)
+        {
+            return "You called GetById";
+        }
+    }
+}
---
+++ UrlHelperTest/UrlHelperTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UrlHelperTest.WebApiApplication" Language="C#" %>
